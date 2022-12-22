ae34facaabba8e4b08fb5f65a736631102db9e47
ransomware3 <natanbaldo11@gmail.com>
2022-12-22 12:00:37 -0300

    resolvidos bugs de caminhos das pastas

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-import { getUser } from '/src/js/services/user.js'
-import { getRepositories } from '/src/js/services/repositories.js'
-import { getEvents } from '/src/js/services/events.js'
-import { user } from '/src/js/objects/user.js'
-import { screen } from '/src/js/objects/screen.js'
+import { getUser } from './services/user.js'
+import { getRepositories } from './services/repositories.js'
+import { getEvents } from './services/events.js'
+import { user } from './objects/user'
+import { screen } from './objects/screen'
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
